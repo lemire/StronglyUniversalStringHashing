48e7d1b481da081406050bd23197e8d4c2f139f8
Daniel Lemire <lemire@gmail.com>
2014-07-21 16:34:30 -0400

    Experiments

--- include/clmulpoly64bits.h
+++ include/clmulpoly64bits.h
@@ -698,173 +698,118 @@ uint64_t halfhashGaloisFieldPoly64_16(const void* rs, const uint64_t * string,
 // PURE GARBAGE (just to test out ideas on the fly)
 uint64_t clmulgarbage(const void* rs, const uint64_t * string,
 		const size_t length) {
-	const uint64_t * randomsource = (const uint64_t *) rs;
-	assert(*randomsource != 0); //otherwise silly
+	const __m128i * randomsource = (const __m128i *) rs;
+	// 4 128-bit is a cache line!!!
+	__m128i key1 = _mm_lddqu_si128( randomsource);
+	__m128i key2 = _mm_lddqu_si128( randomsource + 1 );
+	__m128i key3 = _mm_lddqu_si128( randomsource + 2 );
+	__m128i key4 = _mm_lddqu_si128( randomsource + 3 );
+
 	const uint64_t * const endstring = string + length;
-	__m128i tkey1 = _mm_set_epi64x(0, *(randomsource));
-	__m128i acc = _mm_set_epi64x(0, *string);
-	++string;
-	if (string + 1 < endstring) {
-		// we start by precomputing the powers of the key
-		__m128i tkey2 = precompReduction64_si128(
-				_mm_clmulepi64_si128(tkey1, tkey1, 0x00));
-		// powers of the keys are packed into two registers
-		__m128i key = _mm_xor_si128(_mm_and_si128(tkey1, _mm_set_epi64x(0, -1)),
-				_mm_slli_si128(tkey2, 8));
-		if (string + 3 < endstring) {
-			__m128i tkey3 = precompReduction64_si128(
-					_mm_clmulepi64_si128(tkey2, tkey1, 0x00));
-			__m128i tkey4 = precompReduction64_si128(
-					_mm_clmulepi64_si128(tkey2, tkey2, 0x00));
-			__m128i key2 = _mm_xor_si128(
-					_mm_and_si128(tkey3, _mm_set_epi64x(0, -1)),
-					_mm_slli_si128(tkey4, 8));
-			if (string + 31 < endstring) {
-				__m128i tkey5 = precompReduction64_si128(
-						_mm_clmulepi64_si128(tkey2, tkey3, 0x00));
-				__m128i tkey6 = precompReduction64_si128(
-						_mm_clmulepi64_si128(tkey3, tkey3, 0x00));
-				__m128i tkey7 = precompReduction64_si128(
-						_mm_clmulepi64_si128(tkey3, tkey4, 0x00));
-				__m128i tkey8 = precompReduction64_si128(
-						_mm_clmulepi64_si128(tkey4, tkey4, 0x00));
-				__m128i tkey9 = precompReduction64_si128(
-						_mm_clmulepi64_si128(tkey5, tkey4, 0x00));
-				__m128i tkey10 = precompReduction64_si128(
-						_mm_clmulepi64_si128(tkey5, tkey5, 0x00));
-				__m128i tkey11 = precompReduction64_si128(
-						_mm_clmulepi64_si128(tkey6, tkey5, 0x00));
-				__m128i tkey12 = precompReduction64_si128(
-						_mm_clmulepi64_si128(tkey6, tkey6, 0x00));
-				__m128i tkey13 = precompReduction64_si128(
-						_mm_clmulepi64_si128(tkey6, tkey7, 0x00));
-				__m128i tkey14 = precompReduction64_si128(
-						_mm_clmulepi64_si128(tkey7, tkey7, 0x00));
-				__m128i tkey15 = precompReduction64_si128(
-						_mm_clmulepi64_si128(tkey8, tkey7, 0x00));
-				__m128i tkey16 = precompReduction64_si128(
-						_mm_clmulepi64_si128(tkey8, tkey8, 0x00));
-				__m128i key3 = _mm_xor_si128(
-						_mm_and_si128(tkey5, _mm_set_epi64x(0, -1)),
-						_mm_slli_si128(tkey6, 8));
-				__m128i key4 = _mm_xor_si128(
-						_mm_and_si128(tkey7, _mm_set_epi64x(0, -1)),
-						_mm_slli_si128(tkey8, 8));
-				__m128i key5 = _mm_xor_si128(
-						_mm_and_si128(tkey9, _mm_set_epi64x(0, -1)),
-						_mm_slli_si128(tkey10, 8));
-				__m128i key6 = _mm_xor_si128(
-						_mm_and_si128(tkey11, _mm_set_epi64x(0, -1)),
-						_mm_slli_si128(tkey12, 8));
-				__m128i key7 = _mm_xor_si128(
-						_mm_and_si128(tkey13, _mm_set_epi64x(0, -1)),
-						_mm_slli_si128(tkey14, 8));
-				__m128i key8 = _mm_xor_si128(
-						_mm_and_si128(tkey15, _mm_set_epi64x(0, -1)),
-						_mm_slli_si128(tkey16, 8));
 
-				/**
-				 * For the half multiplication thing, we want to have the keys organized
-				 * as follows:
-				 * (0,k),(k^2,k^3)...
-				 */
-				// TODO: these keys are probably wrong
-				__m128i hkey1 = _mm_slli_si128(key,8);
-				__m128i hkey2 = _mm_xor_si128(_mm_srli_si128(key,8), _mm_slli_si128(key2,8));
-				__m128i hkey3 = _mm_xor_si128(_mm_srli_si128(key2,8), _mm_slli_si128(key3,8));
-				__m128i hkey4 = _mm_xor_si128(_mm_srli_si128(key3,8), _mm_slli_si128(key4,8));
-				__m128i hkey5 = _mm_xor_si128(_mm_srli_si128(key4,8), _mm_slli_si128(key5,8));
-				__m128i hkey6 = _mm_xor_si128(_mm_srli_si128(key5,8), _mm_slli_si128(key6,8));
-				__m128i hkey7 = _mm_xor_si128(_mm_srli_si128(key6,8), _mm_slli_si128(key7,8));
-				__m128i hkey8 = _mm_xor_si128(_mm_srli_si128(key7,8), _mm_slli_si128(key8,8));
+	__m128i acc = _mm_setzero_si128();
+	for(;string + 7 < endstring;string+=8) {
+		__m128i p1 = _mm_clmulepi64_si128(acc, key1, 0x00);
 
-				for (; string + 31 < endstring; string += 32) {
+		__m128i temp1 = _mm_lddqu_si128((__m128i *) string);
+		__m128i p2 = _mm_clmulepi64_si128(temp1, key1, 0x01);
+		__m128i p3 = _mm_clmulepi64_si128(temp1, key2, 0x00);
 
-					__m128i temp = _mm_lddqu_si128((__m128i *) string); //a1 a2
-					__m128i temp2 = _mm_lddqu_si128((__m128i *) (string + 2)); //a3 a4
-					__m128i temp3 = _mm_lddqu_si128((__m128i *) (string + 4)); //a5 a6
-					__m128i temp4 = _mm_lddqu_si128((__m128i *) (string + 6)); //a7 a8
-					__m128i temp5 = _mm_lddqu_si128((__m128i *) (string + 8)); //a7 a8
-					__m128i temp6 = _mm_lddqu_si128((__m128i *) (string + 10)); //a7 a8
-					__m128i temp7 = _mm_lddqu_si128((__m128i *) (string + 12)); //a7 a8
-					__m128i temp8 = _mm_lddqu_si128((__m128i *) (string + 14)); //a7 a8
+		__m128i temp2 = _mm_lddqu_si128((__m128i *) (string + 2));
+		__m128i p4 = _mm_clmulepi64_si128(temp2, key2, 0x01);
+		__m128i p5 = _mm_clmulepi64_si128(temp2, key3, 0x00);
 
-					__m128i temp9 = _mm_lddqu_si128((__m128i *) (string + 16)); //a1 a2
-					__m128i temp10 = _mm_lddqu_si128((__m128i *) (string + 18)); //a3 a4
-					__m128i temp11 = _mm_lddqu_si128((__m128i *) (string + 20)); //a5 a6
-					__m128i temp12 = _mm_lddqu_si128((__m128i *) (string + 22)); //a7 a8
-					__m128i temp13 = _mm_lddqu_si128((__m128i *) (string + 24)); //a7 a8
-					__m128i temp14 = _mm_lddqu_si128((__m128i *) (string + 26)); //a7 a8
-					__m128i temp15 = _mm_lddqu_si128((__m128i *) (string + 28)); //a7 a8
-					__m128i temp16 = _mm_lddqu_si128((__m128i *) (string + 30)); //a7 a8
+		__m128i temp3 = _mm_lddqu_si128((__m128i *) (string + 4));
+		__m128i p6 = _mm_clmulepi64_si128(temp3, key3, 0x01);
+		__m128i p7 = _mm_clmulepi64_si128(temp3, key4, 0x00);
+
+		__m128i temp4 = _mm_lddqu_si128((__m128i *) (string + 6));
+		__m128i p8 = _mm_clmulepi64_si128(temp4, key4, 0x01);
+		__m128i p9 = _mm_slli_si128(temp4, 8);
 
 
-					__m128i t1 = _mm_xor_si128(hkey1,temp);
-					__m128i t2 = _mm_xor_si128(hkey2,temp2);
-					__m128i t3 = _mm_xor_si128(hkey3,temp3);
-					__m128i t4 = _mm_xor_si128(hkey4,temp4);
-					__m128i t5 = _mm_xor_si128(hkey5,temp);
-					__m128i t6 = _mm_xor_si128(hkey6,temp2);
-					__m128i t7 = _mm_xor_si128(hkey7,temp3);
-					__m128i t8 = _mm_xor_si128(hkey8,temp4);
-					__m128i clprod1 = _mm_clmulepi64_si128(t1,t1,0x10);
-					__m128i clprod2 = _mm_clmulepi64_si128(t2,t2,0x10);
-					__m128i clprod3 = _mm_clmulepi64_si128(t3,t3,0x10);
-					__m128i clprod4 = _mm_clmulepi64_si128(t4,t4,0x10);
-					__m128i clprod5 = _mm_clmulepi64_si128(t5,t5,0x10);
-					__m128i clprod6 = _mm_clmulepi64_si128(t6,t6,0x10);
-					__m128i clprod7 = _mm_clmulepi64_si128(t7,t7,0x10);
-					__m128i clprod8 = _mm_clmulepi64_si128(t8,t8,0x10);
-					__m128i tacc = _mm_clmulepi64_si128(acc, key8, 0x10);
-					const __m128i b1 = _mm_xor_si128(clprod2, clprod1);
-					const __m128i b2 = _mm_xor_si128(clprod3, clprod4);
-					const __m128i b3 = _mm_xor_si128(clprod5, clprod6);
-					const __m128i b4 = _mm_xor_si128(clprod7, clprod8);
-					const __m128i z1 = _mm_xor_si128(b1, b2);
-					const __m128i z2 = _mm_xor_si128(b3, b4);
-					const __m128i Z1 = _mm_xor_si128(z1, z2);
-					acc = //precompReduction64_si128
-							(
-							_mm_xor_si128(tacc, Z1));
-					acc = _mm_xor_si128(acc,temp9);
-					acc = _mm_xor_si128(acc,temp10);
-					acc = _mm_xor_si128(acc,temp11);
-					acc = _mm_xor_si128(acc,temp12);
-					acc = _mm_xor_si128(acc,temp13);
-					acc = _mm_xor_si128(acc,temp14);
-					acc = _mm_xor_si128(acc,temp15);
-					acc = precompReduction64_si128(acc);
-				}
-			}
-			for (; string + 3 < endstring; string += 4) {
-				__m128i temp = _mm_lddqu_si128((__m128i *) string); //a1 a2
-				__m128i temp2 = _mm_lddqu_si128((__m128i *) (string + 2)); //a3 a4
-				const __m128i x1 = _mm_srli_si128(temp2, 8); //a4
-				const __m128i clprod1 = _mm_clmulepi64_si128(temp, key2, 0x00); //a1*k^3
-				const __m128i clprod2 = _mm_clmulepi64_si128(temp, key, 0x11); //a2*k^2
-				const __m128i clprod3 = _mm_clmulepi64_si128(temp2, key, 0x00); //a3*k
-				acc = _mm_clmulepi64_si128(acc, key2, 0x10); //k^4
-				acc = precompReduction64_si128
-						(
-						_mm_xor_si128(acc,
-								_mm_xor_si128(_mm_xor_si128(x1, clprod1),
-										_mm_xor_si128(clprod2, clprod3))));
-			}
-		}
-		for (; string + 1 < endstring; string += 2) {
-			__m128i temp = _mm_lddqu_si128((__m128i *) string);
-			acc = _mm_clmulepi64_si128(acc, key, 0x10);
-			const __m128i clprod1 = _mm_clmulepi64_si128(temp, key, 0x00);
-			acc = _mm_xor_si128(clprod1, acc);
-			acc = _mm_xor_si128(acc, _mm_srli_si128(temp, 8));
-			acc = precompReduction64_si128(acc);
-		}
+		__m128i q1 = _mm_xor_si128(p1,p2);
+		__m128i q2 = _mm_xor_si128(p3,p4);
+		__m128i q3 = _mm_xor_si128(p5,p6);
+		__m128i q4 = _mm_xor_si128(p7,p8);
+
+		__m128i r1 = _mm_xor_si128(q1,q2);
+		__m128i r2 = _mm_xor_si128(q3,q4);
+		__m128i s1 = _mm_xor_si128(r1,r2);
+		__m128i s2 = _mm_xor_si128(s1,p9);
+		acc = s2;//precompReduction64_si128(s2);
 	}
-	if (string < endstring) {
-		const __m128i temp = _mm_set_epi64x(0, *string);
-		const __m128i multi = _mm_clmulepi64_si128(acc, tkey1, 0x00);
-		acc = precompReduction64_si128(multi);
-		acc = _mm_xor_si128(acc, temp);
+
+	for (;string + 1 < endstring;string+=2) {
+		__m128i p1 = _mm_clmulepi64_si128(acc, key1, 0x00);
+		__m128i temp1 = _mm_lddqu_si128((__m128i *) string);
+		__m128i p2 = _mm_clmulepi64_si128(temp1, key1, 0x01);
+		__m128i p3 = _mm_slli_si128(temp1, 8);
+		acc = precompReduction64_si128(_mm_xor_si128(_mm_xor_si128(p1,p2),p3));
+	}
+	if(string<endstring){
+		__m128i p1 = _mm_clmulepi64_si128(acc, key1, 0x00);
+		acc = precompReduction64_si128(_mm_xor_si128(p1,_mm_set_epi64x(0, *string)));
+	}
+	return _mm_cvtsi128_si64(acc);
+}
+
+
+// PURE GARBAGE (just to test out ideas on the fly)
+uint64_t clmulgarbage2(const void* rs, const uint64_t * string,
+		const size_t length) {
+	const __m128i * randomsource = (const __m128i *) rs;
+	// 4 128-bit is a cache line!!!
+	__m128i key1 = _mm_lddqu_si128( randomsource);
+	__m128i key2 = _mm_lddqu_si128( randomsource + 1 );
+	__m128i key3 = _mm_lddqu_si128( randomsource + 2 );
+	__m128i key4 = _mm_lddqu_si128( randomsource + 3 );
+
+	const uint64_t * const endstring = string + length;
+
+	__m128i acc = _mm_setzero_si128();
+	for(;string + 7 < endstring;string+=8) {
+		__m128i p1 = _mm_clmulepi64_si128(acc, key1, 0x00);
+
+		__m128i temp1 = _mm_lddqu_si128((__m128i *) string);
+		__m128i p2 = _mm_clmulepi64_si128(temp1, key1, 0x01);
+		__m128i p3 = _mm_clmulepi64_si128(temp1, key2, 0x00);
+
+		__m128i temp2 = _mm_lddqu_si128((__m128i *) (string + 2));
+		__m128i p4 = _mm_clmulepi64_si128(temp2, key2, 0x01);
+		__m128i p5 = _mm_clmulepi64_si128(temp2, key3, 0x00);
+
+		__m128i temp3 = _mm_lddqu_si128((__m128i *) (string + 4));
+		__m128i p6 = _mm_clmulepi64_si128(temp3, key3, 0x01);
+		__m128i p7 = _mm_clmulepi64_si128(temp3, key4, 0x00);
+
+		__m128i temp4 = _mm_lddqu_si128((__m128i *) (string + 6));
+		__m128i p8 = _mm_clmulepi64_si128(temp4, key4, 0x01);
+		__m128i p9 = _mm_slli_si128(temp4, 8);
+
+
+		__m128i q1 = _mm_xor_si128(p1,p2);
+		__m128i q2 = _mm_xor_si128(p3,p4);
+		__m128i q3 = _mm_xor_si128(p5,p6);
+		__m128i q4 = _mm_xor_si128(p7,p8);
+
+		__m128i r1 = _mm_xor_si128(q1,q2);
+		__m128i r2 = _mm_xor_si128(q3,q4);
+		__m128i s1 = _mm_xor_si128(r1,r2);
+		__m128i s2 = _mm_xor_si128(s1,p9);
+		acc = precompReduction64_si128(s2);
+	}
+
+	for (;string + 1 < endstring;string+=2) {
+		__m128i p1 = _mm_clmulepi64_si128(acc, key1, 0x00);
+		__m128i temp1 = _mm_lddqu_si128((__m128i *) string);
+		__m128i p2 = _mm_clmulepi64_si128(temp1, key1, 0x01);
+		__m128i p3 = _mm_slli_si128(temp1, 8);
+		acc = precompReduction64_si128(_mm_xor_si128(_mm_xor_si128(p1,p2),p3));
+	}
+	if(string<endstring){
+		__m128i p1 = _mm_clmulepi64_si128(acc, key1, 0x00);
+		acc = precompReduction64_si128(_mm_xor_si128(p1,_mm_set_epi64x(0, *string)));
 	}
 	return _mm_cvtsi128_si64(acc);
 }
