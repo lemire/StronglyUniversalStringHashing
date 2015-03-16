a3563b9379fd05c92a0d502d26bee30c6c9c2a7e
Daniel Lemire <lemire@gmail.com>
2015-03-16 14:34:56 -0400

    Pushing...

--- include/clmulhierarchical64bits.h
+++ include/clmulhierarchical64bits.h
@@ -58,6 +58,64 @@ static __m128i __clmulhalfscalarproductwithoutreduction(const __m128i * randomso
 	return acc;
 }
 
+static __m128i __clmulhalfscalarproductwithoutreduction16(const __m128i * randomsource, const uint64_t * string) {
+	//assert(((uintptr_t) randomsource & 15) == 0);// we expect cache line alignment for the keys
+	// we expect length = 128, so we need  16 cache lines of keys and 16 cache lines of strings.
+	//if(CLHASH_DEBUG) assert((length & 3) == 0); // if not, we need special handling (omitted)
+	//const uint64_t * const endstring = string + length;
+	__m128i acc = _mm_setzero_si128();
+	// we expect length = 128
+	//for (; string + 3 < endstring; randomsource += 2, string += 4) {
+	__m128i temp1, temp2, add1, clprod1,temp12,temp22,add12,clprod12;
+        temp1 = _mm_load_si128( randomsource);
+		temp2 = _mm_lddqu_si128((__m128i *) string);
+		add1 = _mm_xor_si128(temp1, temp2);
+		clprod1 = _mm_clmulepi64_si128(add1, add1, 0x10);
+		acc = _mm_xor_si128(clprod1, acc);
+		temp12 = _mm_load_si128(randomsource + 1);
+		temp22 = _mm_lddqu_si128((__m128i *) (string + 2));
+		add12 = _mm_xor_si128(temp12, temp22);
+		clprod12 = _mm_clmulepi64_si128(add12, add12, 0x10);
+		acc = _mm_xor_si128(clprod12, acc);
+
+        temp1 = _mm_load_si128( randomsource + 2);
+		temp2 = _mm_lddqu_si128((__m128i *) (string + 4));
+		add1 = _mm_xor_si128(temp1, temp2);
+		clprod1 = _mm_clmulepi64_si128(add1, add1, 0x10);
+		acc = _mm_xor_si128(clprod1, acc);
+		temp12 = _mm_load_si128(randomsource + 3);
+		temp22 = _mm_lddqu_si128((__m128i *) (string + 6));
+		add12 = _mm_xor_si128(temp12, temp22);
+		clprod12 = _mm_clmulepi64_si128(add12, add12, 0x10);
+		acc = _mm_xor_si128(clprod12, acc);
+
+        temp1 = _mm_load_si128( randomsource + 4);
+		temp2 = _mm_lddqu_si128((__m128i *) (string + 8));
+		add1 = _mm_xor_si128(temp1, temp2);
+		clprod1 = _mm_clmulepi64_si128(add1, add1, 0x10);
+		acc = _mm_xor_si128(clprod1, acc);
+		temp12 = _mm_load_si128(randomsource + 5);
+		temp22 = _mm_lddqu_si128((__m128i *) (string + 10));
+		add12 = _mm_xor_si128(temp12, temp22);
+		clprod12 = _mm_clmulepi64_si128(add12, add12, 0x10);
+		acc = _mm_xor_si128(clprod12, acc);
+
+        temp1 = _mm_load_si128( randomsource + 6);
+		temp2 = _mm_lddqu_si128((__m128i *) (string + 12));
+		add1 = _mm_xor_si128(temp1, temp2);
+		clprod1 = _mm_clmulepi64_si128(add1, add1, 0x10);
+		acc = _mm_xor_si128(clprod1, acc);
+		temp12 = _mm_load_si128(randomsource + 7);
+		temp22 = _mm_lddqu_si128((__m128i *) (string + 14));
+		add12 = _mm_xor_si128(temp12, temp22);
+		clprod12 = _mm_clmulepi64_si128(add12, add12, 0x10);
+		acc = _mm_xor_si128(clprod12, acc);
+
+
+		///}
+//if(CLHASH_DEBUG) assert(string == endstring);
+	return acc;
+}
 
 // the value length does not have to be divisible by 4
 static __m128i __clmulhalfscalarproductwithtailwithoutreduction(const __m128i * randomsource,
@@ -224,13 +282,13 @@ uint64_t CLHASH16(const void* rs, const uint64_t * string,
 	polyvalue = _mm_and_si128(polyvalue,_mm_setr_epi32(0xFFFFFFFF,0xFFFFFFFF,0xFFFFFFFF,0x3fffffff));// setting two highest bits to zero
 	// we should check that polyvalue is non-zero, though this is best done outside the function and highly unlikely
 	if (m < length) { // long strings
-		__m128i  acc =  __clmulhalfscalarproductwithoutreduction(rs64, string,m);
+		__m128i  acc =  __clmulhalfscalarproductwithoutreduction16(rs64, string);
 		size_t t = m;
 		for (; t +  m <= length; t +=  m) {
 			// we compute something like
 			// acc+= polyvalue * acc + h1
 			acc =  mul128by128to128_lazymod127(polyvalue,acc);
-			const __m128i h1 =  __clmulhalfscalarproductwithoutreduction(rs64, string+t,m);
+			const __m128i h1 =  __clmulhalfscalarproductwithoutreduction16(rs64, string+t);
 			acc = _mm_xor_si128(acc,h1);
 		}
 		const int remain = length - t;
